23dfe528d9345c1d41484264428325b6b2234178
suebin <suuebin@gmail.com>
2023-06-03 18:42:00 +0900

    Initial commit

---
+++ src/main/java/com/nhnacademy/minidoorayaccountapi/MiniDoorayAccountApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidoorayaccountapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoorayAccountApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDoorayAccountApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/minidoorayaccountapi/MiniDoorayAccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidoorayaccountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDoorayAccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
