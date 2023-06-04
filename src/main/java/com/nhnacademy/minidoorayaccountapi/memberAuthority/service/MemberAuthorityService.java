d2104477c65e624da668cc1757ebf65cb138dd0a
장현준 <gw0406@naver.com>
2023-06-04 16:33:16 +0900

    Jun235 modify memberAuthority

---
+++ src/main/java/com/nhnacademy/minidoorayaccountapi/memberAuthority/service/MemberAuthorityService.java
@@ -0,0 +1,9 @@
+package com.nhnacademy.minidoorayaccountapi.memberAuthority.service;
+
+import com.nhnacademy.minidoorayaccountapi.member.dto.MemberDto;
+
+import java.util.List;
+
+public interface MemberAuthorityService {
+
+}
