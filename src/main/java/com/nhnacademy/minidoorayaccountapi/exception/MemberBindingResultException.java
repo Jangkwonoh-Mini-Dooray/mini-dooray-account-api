124f1361e127c6a3d9ee573a665bb592746902bf
장현준 <gw0406@naver.com>
2023-06-09 13:48:41 +0900

    🔨 refactor(member): member controller 일부 수정

---
+++ src/main/java/com/nhnacademy/minidoorayaccountapi/exception/MemberBindingResultException.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.minidoorayaccountapi.exception;
+
+public class MemberBindingResultException extends RuntimeException {
+    public MemberBindingResultException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/nhnacademy/minidoorayaccountapi/member/dto/MemberIdDto.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.minidoorayaccountapi.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberIdDto {
+    private String memberId;
+}
